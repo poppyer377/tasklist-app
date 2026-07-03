5c0673efe0092cf10a0665bfe97bf76c9ddc19cd
poppy <poppyyu885@gmail.com>
2026-07-03 16:08:05 +1000

    Added login screen

--- App.js
+++ App.js
@@ -17,4 +17,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+
